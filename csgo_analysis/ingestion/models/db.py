71fbe06448ceb254c1a9e86b80d3b497c81340e1
James Trew <j.trew10@gmail.com>
2021-02-28 15:01:23 -0500

    Handle different db operations and insert returns.

--- csgo_analysis/ingestion/models/db.py
+++ csgo_analysis/ingestion/models/db.py
@@ -7,13 +7,27 @@ from psycopg2.extras import execute_values
 
 class DB:
 
+    _OPSTR = 'opstr'
+    _VAL = 'val'
+    _MANY = 'many'
+    _RETURNING = 'returning'
+
     timestamp_fmt = '%Y-%m-%d %H:%M:%S'
 
     def __init__(self):
         cred_path = os.path.join(definition.ING_DIR, 'models', 'db.json')
         self.connection_params = json.loads(open(cred_path).read())
+        self.return_id = None
 
-    def insert(self, tablename, col, val, many=False):
+    def connect(self):
+        self.conn = psycopg2.connect(**self.connection_params)
+        self.cur = self.conn.cursor()
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
+
+    def insert(self, tablename, col, val, many=False, returning=False):
         if many:
             col_holder = '(' + ', '.join(col[0]) + ')'
             val_holder = '%s'
@@ -22,19 +36,37 @@ class DB:
             val_holder = '(' + ', '.join(['%s'] * len(val)) + ')'
         insert_str = f'INSERT INTO {tablename} {col_holder} VALUES {val_holder}'
 
-        print(insert_str, val)
-        self.execute(insert_str, val, many)
+        if returning:
+            insert_str += ' RETURNING id'
 
-    def execute(self, exe_str, val, many):
+        kwargs = {
+            self._OPSTR: insert_str,
+            self._VAL: val,
+            self._MANY: many,
+            self._RETURNING: returning
+        }
+
+        self.execute(self.insert.__name__, kwargs)
+        return self.return_id
+
+    def execute(self, func, kwargs):
+        self.connect()
+        func_call = getattr(DB, self.execute.__name__ + '_' + func)
+        func_call(self, kwargs)
+
+    def execute_insert(self, kwargs):
         # TODO add UniqueViolation error handling
-        self.conn = psycopg2.connect(**self.connection_params)
-        self.cur = self.conn.cursor()
+        insert_str = kwargs.get(self._OPSTR)
+        val = kwargs.get(self._VAL)
+        many = kwargs.get(self._MANY)
+        returning = kwargs.get(self._RETURNING)
 
         if many:
-            execute_values(self.cur, exe_str, val)
+            execute_values(self.cur, insert_str, val)
         else:
-            self.cur.execute(exe_str, val)
+            self.cur.execute(insert_str, val)
         self.conn.commit()
 
-        self.cur.close()
-        self.conn.close()
+        self.return_id = self.cur.fetchone() if returning else None
+
+        self.close()
