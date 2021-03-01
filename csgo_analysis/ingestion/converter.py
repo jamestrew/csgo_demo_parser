011093b7d22da91f9bc9e29d4e021c0e981bde7d
James Trew <j.trew10@gmail.com>
2021-02-28 21:12:53 -0500

    Fix converter bugs.

--- csgo_analysis/ingestion/converter.py
+++ csgo_analysis/ingestion/converter.py
@@ -31,6 +31,11 @@ class JsonConverter:
     new_line_p = r'\n'
     blank = r''
 
+    junk_p = r'}\n(.+\n(?!{))+(.+\n(?={))*?'
+    junk_r = r'}\n'
+
+    announcements = r'[a-z_]+\n{\n}\n'
+
     null_p = r':  '
     null_r = r': "None"'
     tight_null_p = r':\n'
@@ -41,7 +46,7 @@ class JsonConverter:
 
     event_p = r'event\n{\n( [a-zA-Z0-9: \.\(\)_#\-]+\n)+}'
 
-    def convert_2(self, txt, file_path, save=False):
+    def convert(self, txt, file_path, save=False):
         """Convert txt demo data into Python object (list of dict).
 
         Parameters
@@ -65,11 +70,15 @@ class JsonConverter:
         txt = txt.replace(first_table[0], '')
 
         txt = re.sub(self.junk_p, self.junk_r, txt)
-        txt = re.sub(self.annoucements, self.blank, txt)
+        txt = re.sub(self.announcements, self.blank, txt)
 
         for event in Events.UNNECESSARY_DATA:
             pattern = self.event_p.replace('event', event)
             txt = re.sub(pattern, self.blank, txt)
+
+        save_path = file_path.replace('.txt', '_events.txt')
+        with open(save_path, 'w') as file:
+            file.write(txt)
 
         txt = re.sub(self.tight_null_p, self.tight_null_r, txt)
         txt = re.sub(self.inner_key_p, self.inner_key_r, txt)
@@ -80,11 +89,17 @@ class JsonConverter:
         txt = re.sub(self.new_line_p, self.blank, txt)
         txt = re.sub(self.trailing_comma_p, self.trailing_comma_r, txt)
         txt = re.sub(self.trailing_comma_p2, self.trailing_comma_r2, txt)
+        txt = f'[{txt.strip()[:-1]}]'  # convert to array
+
+        # debug
+        save_path = file_path.replace('.txt', '_final.txt')
+        with open(save_path, 'w') as file:
+            file.write(txt)
 
         data = json.loads(txt)
 
         if save:
-            file_path = file_path.replace('txt', '_test.json')
+            file_path = file_path.replace('.txt', '.json')
             with open(file_path, 'w') as jsonfile:
                 json.dump(data, jsonfile)
 
