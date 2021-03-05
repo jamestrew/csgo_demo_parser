c82d9006bc0f6918c1300367aa5f8b5977c39f38
James Trew <j.trew10@gmail.com>
2021-03-04 19:19:48 -0500

    Refactor Db typecasting.

--- csgo_analysis/ingestion/models/game.py
+++ csgo_analysis/ingestion/models/game.py
@@ -6,16 +6,27 @@ from csgo_analysis.ingestion.models.db import DB
 from psycopg2._psycopg import TimestampFromTicks
 
 
-class Game(DBConn):
+class Game(DBConn, DB):
 
-    # columns
-    _ID = {DB._NAME: 'id', DB._TYPE: int}
-    _SHARE_CODE = {DB._NAME: 'share_code', DB._TYPE: str}
-    _MATCH_TIME = {DB._NAME: 'match_time', DB._TYPE: TimestampFromTicks}
-    _MATCH_DURATION = {DB._NAME: 'match_duration', DB._TYPE: int}
-    _MAP_L_ID = {DB._NAME: 'map_l_id', DB._TYPE: int}
-    _FINAL_SCORE_TWO = {DB._NAME: 'final_score_two', DB._TYPE: int}
-    _FINAL_SCORE_THREE = {DB._NAME: 'final_score_three', DB._TYPE: int}
+    # column names
+    _ID = 'id'
+    _SHARE_CODE = 'share_code'
+    _MATCH_TIME = 'match_time'
+    _MATCH_DURATION = 'match_duration'
+    _MAP_L_ID = 'map_l_id'
+    _FINAL_SCORE_TWO = 'final_score_two'
+    _FINAL_SCORE_THREE = 'final_score_three'
+
+    # column types
+    _TYPES = {
+        _ID: int,
+        _SHARE_CODE: str,
+        _MATCH_TIME: TimestampFromTicks,
+        _MATCH_DURATION: int,
+        _MAP_L_ID: int,
+        _FINAL_SCORE_TWO: int,
+        _FINAL_SCORE_THREE: int
+    }
 
     # table
     _TABLE_NAME = 'game'
@@ -27,22 +38,18 @@ class Game(DBConn):
     def __init__(self):
         super().__init__()
         self.map_l = MapL()
+        self.fields = {}
         # team 2 CT start, team 3 T start?
-
-    def convert_epoch_time(self, epoch_time):
-        return time.strftime(self.timestamp_fmt, time.localtime(epoch_time))
 
     def ingest_data(self, dirty_data, scode, map_name):
         match_scores = re.search(self.match_info_p, dirty_data)
         epoch_time = int(re.search(self.matchtime_p, dirty_data).group(1))
 
-        self.fields = {
-            self._SHARE_CODE[DB._NAME]: self._SHARE_CODE[DB._TYPE](scode),
-            self._MATCH_TIME[DB._NAME]: self._MATCH_TIME[DB._TYPE](epoch_time),
-            self._MATCH_DURATION[DB._NAME]: self._MATCH_DURATION[DB._TYPE](match_scores.group(3)),
-            self._MAP_L_ID[DB._NAME]: self._MAP_L_ID[DB._TYPE](self.map_l.get_map_l_id(map_name)),
-            self._FINAL_SCORE_TWO[DB._NAME]: self._FINAL_SCORE_TWO[DB._TYPE](match_scores.group(1)),
-            self._FINAL_SCORE_THREE[DB._NAME]: self._FINAL_SCORE_TWO[DB._TYPE](match_scores.group(2))
-        }
+        self.input_column_data(self._SHARE_CODE, scode)
+        self.input_column_data(self._MATCH_TIME, epoch_time)
+        self.input_column_data(self._MATCH_DURATION, match_scores.group(3))
+        self.input_column_data(self._MAP_L_ID, self.map_l.get_map_l_id(map_name))
+        self.input_column_data(self._FINAL_SCORE_TWO, match_scores.group(1))
+        self.input_column_data(self._FINAL_SCORE_THREE, match_scores.group(2))
 
         return self.insert(self._TABLE_NAME, self.fields, returning=True)[0]
