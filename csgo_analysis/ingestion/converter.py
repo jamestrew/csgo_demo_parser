8c5b11fb4ee8656fb4d1bdff4c4a832d5d818e23
James Trew <j.trew10@gmail.com>
2021-03-07 17:59:06 -0500

    Rework JsonConverter to handle packetentities.

--- csgo_analysis/ingestion/converter.py
+++ csgo_analysis/ingestion/converter.py
@@ -1,52 +1,57 @@
 import json
 import re
-from csgo_analysis.ingestion.const import Events
+from csgo_analysis.ingestion.const import EventTypes
 
 
 class JsonConverter:
 
-    player_p = r'player info'
-    player_r = r'player_info'
+    PLAYER_P = r'player info'
+    PLAYER_R = r'player_info'
 
-    main_key_p = r'([a-z_]+)\n({\n)'
-    main_key_r = r'{"\1": \2'
+    MAIN_KEY_P = r'([a-z_]+)\n({\n)'
+    MAIN_KEY_R = r'{"\1": \2'
 
-    inner_key_p = r' ([0-9a-zA-Z_]+):'
-    inner_key_r = r'"\1":'
+    INNER_KEY_P = r' ([0-9a-zA-Z_]+):'
+    INNER_KEY_R = r'"\1":'
 
-    inner_val_p = r': ?([#\-0-9a-zA-Z\. _\(\):]+)'
-    inner_val_r = r': "\1", '
+    DETAILS_KEY_P = r'  (position|facing):.+\n'
+    TEAM_KEY_P = r' ( team: .+\n)'
+    TEAM_KEY_R = r'\1'
 
-    end_bracket_p = r'}'
-    end_bracket_r = r'}}, '
+    INNER_VAL_P = r': ?([#\-0-9a-zA-Z\. _\(\):]+)'
+    INNER_VAL_R = r': "\1", '
 
-    middle_p = r'},\n(.+)\n{'
-    middle_r = r'}, {'
+    END_BRACKET_P = r'}'
+    END_BRACKET_R = r'}}, '
 
-    trailing_comma_p = r', }}, '
-    trailing_comma_r = r'}}, '
-    trailing_comma_p2 = r'", {"'
-    trailing_comma_r2 = r'"}}, {"'
+    MIDDLE_P = r'},\n(.+)\n{'
+    MIDDLE_R = r'}, {'
 
-    new_line_p = r'\n'
-    blank = r''
+    TRAIL_COMMA_P = r', }}, '
+    TRAIL_COMMA_R = r'}}, '
+    TRAIL_COMMA_P2 = r'", {"'
+    TRAIL_COMMA_R2 = r'"}}, {"'
 
-    junk_p = r'}\n(.+\n(?!{))+(.+\n(?={))*?'
-    junk_r = r'}\n'
+    NEW_LINE_P = r'\n'
+    BLANK = r''
 
-    announcements = r'[a-z_]+\n{\n}\n'
+    JUNK_P = r'}\n(.+\n(?!{))+(.+\n(?={))*?'
+    JUNK_R = r'}\n'
 
-    null_p = r':  '
-    null_r = r': "None"'
-    tight_null_p = r':\n'
-    tight_null_r = r':  \n'
+    ANNOUNCEMENTS = r'[a-z_]+\n{\n}\n'
 
-    stop_r = r'stophere\nplayer_info'
-    first_table_p = r'^.+(stophere)$'
+    NULL_P = r':  '
+    NULL_R = r': "None"'
+    TIGHT_NULL_P = r':\n'
+    TIGHT_NULL_R = r':  \n'
 
-    event_p = r'event\n{\n( [a-zA-Z0-9: \.\(\)_#\-]+\n)+}'
+    STOP_R = r'stophere\nplayer_info'
+    FIRST_TABLE_P = r'^.+(stophere)$'
 
-    def convert(self, txt, file_path, save=False):
+    EVENT_P = r'event\n{\n( [a-zA-Z0-9: \.\(\)_#\-]+\n)+}'
+
+    @classmethod
+    def convert(cls, txt, file_path, save=False):
         """Convert txt demo data into Python object (list of dict).
 
         Parameters
@@ -63,38 +68,33 @@ class JsonConverter:
         list
             game data object (list of dict)
         """
-        txt = re.sub(self.player_p, self.player_r, txt)
-        txt = re.sub(self.player_r, self.stop_r, txt, 1)
 
-        first_table = re.match(self.first_table_p, txt, re.DOTALL | re.MULTILINE)
+        txt = re.sub(cls.PLAYER_P, cls.PLAYER_R, txt)
+        txt = re.sub(cls.PLAYER_R, cls.STOP_R, txt, 1)
+
+        flags = re.DOTALL | re.MULTILINE
+        first_table = re.match(cls.FIRST_TABLE_P, txt, flags)
         txt = txt.replace(first_table[0], '')
 
-        txt = re.sub(self.junk_p, self.junk_r, txt)
-        txt = re.sub(self.announcements, self.blank, txt)
+        txt = re.sub(cls.JUNK_P, cls.JUNK_R, txt)
+        txt = re.sub(cls.ANNOUNCEMENTS, cls.BLANK, txt)
+        txt = re.sub(cls.DETAILS_KEY_P, cls.BLANK, txt)
+        txt = re.sub(cls.TEAM_KEY_P, cls.TEAM_KEY_R, txt)
 
-        for event in Events.UNNECESSARY_DATA:
-            pattern = self.event_p.replace('event', event)
-            txt = re.sub(pattern, self.blank, txt)
+        for event in EventTypes.UNNECESSARY_DATA:
+            pattern = cls.EVENT_P.replace('event', event)
+            txt = re.sub(pattern, cls.BLANK, txt)
 
-        save_path = file_path.replace('.txt', '_events.txt')
-        with open(save_path, 'w') as file:
-            file.write(txt)
-
-        txt = re.sub(self.tight_null_p, self.tight_null_r, txt)
-        txt = re.sub(self.inner_key_p, self.inner_key_r, txt)
-        txt = re.sub(self.inner_val_p, self.inner_val_r, txt)
-        txt = re.sub(self.end_bracket_p, self.end_bracket_r, txt)
-        txt = re.sub(self.main_key_p, self.main_key_r, txt)
-        txt = re.sub(self.middle_p, self.middle_r, txt)
-        txt = re.sub(self.new_line_p, self.blank, txt)
-        txt = re.sub(self.trailing_comma_p, self.trailing_comma_r, txt)
-        txt = re.sub(self.trailing_comma_p2, self.trailing_comma_r2, txt)
+        txt = re.sub(cls.TIGHT_NULL_P, cls.TIGHT_NULL_R, txt)
+        txt = re.sub(cls.INNER_KEY_P, cls.INNER_KEY_R, txt)
+        txt = re.sub(cls.INNER_VAL_P, cls.INNER_VAL_R, txt)
+        txt = re.sub(cls.END_BRACKET_P, cls.END_BRACKET_R, txt)
+        txt = re.sub(cls.MAIN_KEY_P, cls.MAIN_KEY_R, txt)
+        txt = re.sub(cls.MIDDLE_P, cls.MIDDLE_R, txt)
+        txt = re.sub(cls.NEW_LINE_P, cls.BLANK, txt)
+        txt = re.sub(cls.TRAIL_COMMA_P, cls.TRAIL_COMMA_R, txt)
+        txt = re.sub(cls.TRAIL_COMMA_P2, cls.TRAIL_COMMA_R2, txt)
         txt = f'[{txt.strip()[:-1]}]'  # convert to array
-
-        # debug
-        save_path = file_path.replace('.txt', '_final.txt')
-        with open(save_path, 'w') as file:
-            file.write(txt)
 
         data = json.loads(txt)
 
---
+++ tests/ingestion/test_converter.py
@@ -0,0 +1,10 @@
+from csgo_analysis.ingestion.converter import JsonConverter
+
+
+def test_conversion():
+    path = 'csgo_analysis/ingestion/data/match730_003464283241853223049_1489324615_121.txt'
+
+    with open(path, encoding='ascii', errors='ignore') as txt:
+        data = txt.read()
+
+    jc = JsonConverter.convert(data, path, True)  # noqa
