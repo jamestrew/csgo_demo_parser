552fef6aa4441589ab81a687ebff58eaf3f6ec00
James Trew <j.trew10@gmail.com>
2021-03-20 20:00:36 -0400

    Test converter.

--- tests/ingestion/test_converter.py
+++ tests/ingestion/test_converter.py
@@ -4,3 +4,8 @@ from csgo_analysis.ingestion.converter import JsonConverter
 def test_begin_new_match(data_txt_begin, data_json_begin):
     output = JsonConverter.convert(data_txt_begin, None, False)
     assert output == data_json_begin
+
+
+def test_n_unicode_match(unicode_txt, unicode_json):
+    output = JsonConverter.convert(unicode_txt, None, False)
+    assert output == unicode_json
